4d3af3369b098c55ff7ccbe720f90e546759a5df
abeixiaolu <xiaoluabei@gmail.com>
2025-06-19 00:40:18 +0800

    ✨ add landing page (#50)
    Co-authored-by: abei <abeixiaolu@gmail.com>

--- app.vue
+++ app.vue
@@ -1,15 +1,5 @@
 <template>
-  <div
-    class="flex gap-4 p-4"
-  >
-    <button class="btn btn-outline btn-success">
-      Success
-    </button>
-    <button class="btn btn-outline btn-warning">
-      Warning
-    </button>
-    <button class="btn btn-outline btn-error">
-      Error <Icon name="i-tabler-map-pin-dollar" size="20" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
