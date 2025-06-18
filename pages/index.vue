73e65aa8cecac197a9e094cac8eb8db209b3b6ae
abei <abeixiaolu@gmail.com>
2025-06-19 00:38:41 +0800

    ✨ add landing page

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hero container mx-auto mt-8 min-h-96 bg-base-300">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Travel Log
+        </h1>
+        <p class="py-6">
+          Keep track of your travels and adventures with this simple travel log app. Add locations, photos, and notes to create a digital journal of your journeys.
+        </p>
+        <button class="btn btn-accent">
+          Sign in with GitHub <Icon name="i-tabler-brand-github" size="24" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
